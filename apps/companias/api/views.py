139699e2ea578b7cd453e2fd9ea710fae1ba1ee2
Luca Citta <lucacitta@gmail.com>
2021-11-25 16:35:45 +0100

    Endpoint por size listo

---
+++ apps/companias/api/views.py
@@ -0,0 +1,8 @@
+from rest_framework import generics
+
+from .serializers import CompaniesSerializer
+from apps.companias.models import Compania
+
+class CompaniesBySize(generics.ListAPIView):
+    queryset = Compania.objects.order_by('-sizeProm')
+    serializer_class = CompaniesSerializer
--- apps/companias/urls.py
+++ apps/companias/urls.py
@@ -1,6 +1,9 @@
 from django.urls.conf import path
+
+
+from .api.views import CompaniesBySize
 from .api import views
 
 urlpatterns = [
-
+    path('bysize/', CompaniesBySize.as_view(), name = 'CompaniesBySize')
 ]
